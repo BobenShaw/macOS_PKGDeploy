cbe7af49716f2f1a7bc3b6c0f5272170cb086a97
Bob Shaw <toxiclightning@gmail.com>
2017-10-19 18:14:47 +0300

    Add files via upload

---
+++ macOS_PKGDeploy.py
@@ -0,0 +1,23 @@
+# Bob's Magic silent multi-part deployment script!
+# Find the source at: https://github.com/IchBinAsuka/macOS_PKGDeploy
+# Written for applications like WebSense that run as PKGs but depend on files sitting with them.
+# Optimized for Python 2.7+ because Apple never updated to modern/clean Python for some reason
+# Hopefully your deployment software of choice will automatically escalate permissions!
+
+# So we can download our payload
+import urllib
+# So we can extract the payload
+import zipfile
+# So we can reference macOS and ask it to politely install the PKG
+import os
+
+# Downloading our payload (Make sure this is accessible!)
+urllib.urlretrieve ("http://thisisafake.server/EXAMPLEPACKAGE.zip", "EXAMPLEPACKAGE.zip")
+
+# Extracting Payload from Package...
+TestPayload = zipfile.ZipFile('EXAMPLEPACKAGE.zip')
+TestPayload.extractall()
+TestPayload.close()
+
+# Time for the pièce de résistance!
+os.system("installer -pkg EXAMPLEPAYLOAD.pkg -target /")
